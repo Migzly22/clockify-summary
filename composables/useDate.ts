2fd9461877b771eda8c350dd4b417241d583129d
CCCI <rolly.migrino02@gmail.com>
2025-02-02 21:24:46 +0800

    refactor: Refactor UseDate composables and remove date_formatter file

--- composables/useDate.ts
+++ composables/useDate.ts
@@ -1,30 +1,57 @@
-import type { DataHolder } from "~/constants/constants"
+export const useDate = () => {
 
+  type FormatDateType = Intl.DateTimeFormatOptions | 'datetime' | 'date' | 'time' | 'time24' | 'YYYY-MM-DD' | 'hh-mm';
+  const dateFormatter = (date?: Date, type?: FormatDateType) => {
+      if (!date) return date;
+      var dt = new Date(date);
 
-export const jsonFormatter = (jsonData : any) : DataHolder => {
-    const start = jsonData?.timeInterval?.start
-    const end = jsonData?.timeInterval?.end
-    const description = jsonData?.description
-    const date = dateTimeFormatter(start)
-    const duration = iso8601(jsonData?.timeInterval?.duration)
-  
-    return {
-      Date: date,
-      dateTime : start,
-      Task: description,
-      Start: dateTimeFormatter(start, "Time"),
-      End: dateTimeFormatter(end, "Time"),
-      Duration: duration
-    }
+      if (type === "datetime")
+          type = { hour12: true, year: "numeric", month: "long", day: "2-digit", hour: "2-digit", minute: "2-digit" };
+      if (type === "date")
+          type = { day: "2-digit", month: "long", year: "numeric", };
+      else if (type === "time")
+          type = { hour12: true, hour: "2-digit", minute: "2-digit", second: "2-digit" };
+      else if (type === "hh-mm")
+          type = { hour12: true, hour: "2-digit", minute: "2-digit", };
+      else if (type === "time24")
+          type = { hour12: false, hour: "2-digit", minute: "2-digit", second: "2-digit" };
+      else if (type === "YYYY-MM-DD")
+          type = { year: "numeric", month: "2-digit", day: "2-digit", };
+
+      if (!type || typeof type === 'string')
+          type = { year: "numeric", month: "long", day: "2-digit" };
+
+      return dt.toLocaleString("en-US", type);
   }
+
+  const getWeekNo = ( date?: Date) =>  {
+    if(!date) return "Week#"
+    const dateData = new Date(date);
   
-export function parseDate(dateString:any) : any {
-      return new Date(dateString.replace('T', ' '));
-  }
+    const dayOfMonth = new Date(dateData.getFullYear(), dateData.getMonth(), 1);
+    const dayOfWeek = dayOfMonth.getDay(); 
+    
+    // Calculate the week number
+    const weekNumber = Math.ceil((dateData.getDate() + dayOfWeek) / 7);
   
-export function iso8601(durationString : string){
-    return ["H","M","S"].map((data:any) => {
-      const inputs = durationString.match(new RegExp("(\\d+)" + data)) 
-      return (inputs ? inputs[1] : "0").padStart(2, '0')
-    }).join(":")
+    return `Week#${weekNumber}`;
   }
+
+  const isoDate = (data:Date | string) =>{
+    const date = new Date(data);
+    const localeDateString = date.toLocaleString('en-US', { 
+        year: 'numeric', 
+        month: '2-digit', 
+        day: '2-digit', 
+    });
+  
+    const [month, day, year] = localeDateString.match(/\d+/g)!;
+    return `${year}-${month}-${day}`;
+  }
+
+  return {
+    dateFormatter,
+    getWeekNo,
+    isoDate
+  }
+}
